4dbce92c30f91456d6e3959b3dbbb2847a3c6d42
Charles Javerliat <charles.javerliat@insa-lyon.fr>
2020-02-06 19:30:09 +0100

    feat/Added fallback support for png if svg is not available

--- scoredl.sh
+++ scoredl.sh
@@ -1,21 +1,72 @@
 #!/bin/bash
 
+function validate_url() {
+  curl --output /dev/null --silent --head --fail "$1"
+}
+
+#TODO vérifier l'install
+
+page=`wget -qO- $1`
+
+if [ $? != 0 ]
+then
+  echo "ERROR : Invalid URL $1"
+  exit 1
+fi
+
+#Répertoire de toutes les pages de la partition
+scores_dir_url=`awk 'match($0, /^<meta property="og:image" content="(.*)score.*@.*">$/, m) {print m[1];}' <<< $page`
+
+if [ $? != 0 ]
+then
+  echo "ERROR : Unable to fetch scores directory."
+  exit 2
+fi
+
+title=`awk 'match($0, /^<meta property="og:title" content="(.*)">$/, m) {print m[1];}' <<< $page`
+
+if [ $? != 0 ]
+then
+  echo "ERROR : Unable to fetch score's title."
+  exit 3
+fi
+
+#Type des fichiers, svg si disponible, sinon png
+score_file_type='png'
+
+if validate_url "${scores_dir_url}score_0.svg"
+then
+  score_file_type='svg'
+fi
+
 i=0
-res=0
+scores=''
 
-`wget -q -O page $1`
-addr=`grep -m1 -oP "https://..*score_0.png" page | rev | cut -c6- | rev`
-nom=`grep -o "<title>.*sheet.*</title>" page | cut -c8- | rev | cut -c60- | rev`
-
-while [ $res = 0 ]
+while [ 1 ]
 do
-	r=`wget -q "${addr}${i}.png"`
-	res=$?
-	i=$((i+1))
+  score_i_file="score_${i}.${score_file_type}"
+  score_i_url="${scores_dir_url}${score_i_file}"
+
+  if validate_url "$score_i_url"
+  then
+    echo "Downloading ${score_i_file} ..."
+    #TODO passer par des variables
+    `wget -q -O "${score_i_file}" "$score_i_url"`
+    echo "Done."
+    echo "Converting ${score_i_file} to score_${i}.pdf ..."
+    `inkscape --export-pdf="score_${i}.pdf" "${score_i_file}"`
+    echo "Done."
+    echo "Removing ${score_i_file} ..."
+    `rm -f "${score_i_file}"`
+    echo "Done."
+    scores="${scores} score_${i}.pdf"
+    i=$((i + 1))
+  else
+    break
+  fi
 done
 
-scores=`ls | grep "score_[0-9]*.png"`
-
-`convert -quiet $scores "${nom}.pdf"` && echo "Finished downloading."
-
-`rm -f page score_[0-9]*.png`
+echo "Combining PDFs."
+`pdfunite $scores "${title}.pdf"`
+echo "Removing tmp files."
+`rm -f $scores`
